a8a656313b9a3b4e5d754fe881de3f93c03a3ae7
KimEilish <jyugo1320@gmail.com>
2020-06-12 06:13:05 +0900

    Project's Initial Commit

---
+++ www/components/menor.js
@@ -0,0 +1,14 @@
+
+
+function sla() {
+  var age=
+  document.getElementById('idade');
+
+  if(age.value>=18){
+  document.getElementById('nsei');
+  document.write('<img src="https://blog.influx.com.br/storage/app/media/uploaded-files/hubfs/1415475/blog-files/maior-menor-de-idade-img1_2.png">');  
+}else{
+  document.write('<img src="https://i2.wp.com/capitalremocoes.com.br/wp-content/uploads/2018/08/menor18.jpg?fit=650%2C397&ssl=1" width="90%">');
+}
+
+}
